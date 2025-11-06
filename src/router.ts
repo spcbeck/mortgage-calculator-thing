c9eba3a4051a4bc81ec791209f2d03242d7610f7
Sean Beck <sean@revivalcoop.com>
2025-11-05 22:56:47 -0500

    adding routes

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,23 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import HomePage from '@/pages/HomePage.vue'
 import AddAssetPage from '@/pages/AddAssetPage.vue'
 import AssetsPage from '@/pages/AssetsPage.vue'
 import EditAssetPage from '@/pages/EditAssetPage.vue'
+import GetMortgageRatesPage from './pages/GetMortgageRatesPage.vue'
+import DetailedAssetsPage from '@/pages/DetailedAssetsPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/asset/edit/:id', component: EditAssetPage },
-  { path: '/asset/add', component: AddAssetPage },
-  { path: '/asset/:id', component: AssetsPage },
+  { path: '/assets', component: AssetsPage },
+  { path: '/assets/edit/:id', component: EditAssetPage, props: true },
+  { path: '/assets/add', component: AddAssetPage },
+  { path: '/assets/:id', component: DetailedAssetsPage, props: true },
+  { path: '/calculate-mortgage', component: GetMortgageRatesPage },
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
